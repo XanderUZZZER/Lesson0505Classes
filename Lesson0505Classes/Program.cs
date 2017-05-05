92cec2f777928b5077f2fae33aca205a5aca4a7b
Oleksandr <xander_uzzzer@mail.ru>
2017-05-05 18:34:59 +0300

    0505171834

---
+++ Lesson0505Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson0505Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lesson 0505 classes");
+
+            Console.ReadLine();
+        }
+    }
+}
